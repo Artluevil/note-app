913aa81da186aa921642f03ebb5434c187563a1a
Artluevil <artluevil139@gmail.com>
2019-07-20 16:45:44 +0200

    Updates

---
+++ precache-manifest.ac3f652e0ccbf1bbe20f0c7f45d48ba3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "300c228deb7238d5072c33e9fb0d9665",
+    "url": "/note-app/index.html"
+  },
+  {
+    "revision": "9e4833fbb60fea4bb970",
+    "url": "/note-app/static/css/2.57797527.chunk.css"
+  },
+  {
+    "revision": "9e4833fbb60fea4bb970",
+    "url": "/note-app/static/js/2.baf3c1c8.chunk.js"
+  },
+  {
+    "revision": "48b635af6786f4ab9c8d",
+    "url": "/note-app/static/js/main.42b9c6ad.chunk.js"
+  },
+  {
+    "revision": "47b98c4c94c3392872d3",
+    "url": "/note-app/static/js/runtime~main.a60a26f2.js"
+  }
+]);
